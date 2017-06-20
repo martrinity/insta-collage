f960d8be455c3a23b7068881e388e70f8486dbbf
martrinity <martiloca_4@hotmail.com>
2017-06-20 12:29:20 -0400

    validaciones

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-main.js
+/*validacion de login*/
+
+var login = document.getElementById("inicia");
+
+inicia.addEventListener("click" , function(){
+	var usuario = document.getElementById("usr").value;
+	var password = document.getElementById("pwd").value;
+
+	if(usuario == ""){
+		alert("Ingrese usuario válido");
+	}
+
+	if (password == "123456" || password == "098765" || password.length <= 6){
+	alert("La contraseña es incorrecta. Ingrese campos validos")
+}
+});
